5cfa13c241301c96e82d3c6a1bba4e60b8c8af6f
Max Smirnov <masynchin@gmail.com>
2022-05-10 23:55:19 +0300

    Add main Editor class

--- editor/index.ts
+++ editor/index.ts
@@ -2,17 +2,29 @@ import { SVG } from "@svgdotjs/svg.js";
 import { WithStrokes } from "./figure/withStroke";
 import { MainStream } from "./stream/main";
 
-const logo = SVG()
-  .addTo(document.getElementById("logo"))
-  .viewbox("0 0 161 125");
+class Editor {
+  private document: Document;
+  private logoElement: HTMLElement;
 
-const stream$ = new MainStream(document);
+  constructor(document: Document, logoElement: HTMLElement) {
+    this.document = document;
+    this.logoElement = logoElement;
+  }
 
-stream$.asObservable().subscribe(([figures, logoSettings]) => {
-  const stroke = {
-    color: logoSettings.stroke,
-    width: +logoSettings.strokeWidth,
-  };
-  logo.clear().width(logoSettings.width).fill("none");
-  new WithStrokes(figures, stroke).render(logo);
-});
+  run(): void {
+    const logo = SVG().addTo(this.logoElement).viewbox("0 0 161 125");
+
+    new MainStream(this.document)
+      .asObservable()
+      .subscribe(([figures, logoSettings]) => {
+        const stroke = {
+          color: logoSettings.stroke,
+          width: +logoSettings.strokeWidth,
+        };
+        logo.clear().width(logoSettings.width).fill("none");
+        new WithStrokes(figures, stroke).render(logo);
+      });
+  }
+}
+
+new Editor(document, document.getElementById("logo")).run();
